1a0f441fa47f1dc9c21d5b9a5d95e3c495a7e810
Rich DiPippo <rjdipippo@gmail.com>
2025-12-17 14:40:51 -0500

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Router, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -11,13 +11,13 @@ import NotFound from "@/pages/not-found";
 
 function Router() {
   return (
-    <Switch>
-      <Route path="/BudgetBuddy" component={Dashboard} />
+    <Router base="/BudgetBuddy">
+      <Route path="/" component={Dashboard} />
       <Route path="/accounts" component={Accounts} />
       <Route path="/transactions" component={Transactions} />
       <Route path="/budgets" component={Budgets} />
       <Route component={NotFound} />
-    </Switch>
+    </Router>
   );
 }
 
